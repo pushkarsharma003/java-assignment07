9bb76ae852939d3ef72d36db4c16007de7a06d10
PushkarSharma <pushkarsharma003@gmail.com>
2018-09-03 20:23:19 +0530

    a07

---
+++ invokeparent.java
@@ -0,0 +1,18 @@
+class parent
+{
+	public void fun() 
+	{		
+		System.out.println("i am a function");	
+	}
+}
+class child extends parent
+{
+	//
+}
+class Main
+{	
+	public static void main(String[] args)
+	{
+		App.fun();
+	}
+}
